32c7304abcacb130690215afcee9eda6dd74ba70
TomGu <tom.guzewich@microsoft.com>
2018-11-08 12:17:50 -0800

    Excluded a bunch of files (I hope).

--- PlayerInboxCloudScript.ts
+++ PlayerInboxCloudScript.ts
@@ -1,69 +1,8 @@
 
-//@todo: move these to some better place for constants? (title data?)
-var MAX_MESSAGES: number = 10;
-//var INBOX_KEY: string = "playerInbox";
-var INBOX_FILENAME: string = "player_inbox.json";
-var INBOX_VERSION: number = 1;
+const MAX_MESSAGES: number = 2400;
+const INBOX_FILENAME: string = "player_inbox.json";
 
-/*
-var SendMessage = function (args: any, context: IPlayFabContext): ISendMessageResponse {
-
-    // The pre-defined "currentPlayerId" variable is initialized to the PlayFab ID of the player logged-in on the game client. 
-    // Cloud Script handles authenticating the player automatically.
-    var message = "Send Message from " + currentPlayerId + "!";
-    //server.GetPlayerProfile.
-
-    // You can use the "log" object to write out debugging statements. It has
-    // three functions corresponding to logging level: debug, info, and error. These functions
-    // take a message string and an optional object.
-    log.info(message);
-    var messageFromRequest = null;
-    if (args && args.message)
-        messageFromRequest = args.message;
-    log.debug("sendMessage:", { input: args.message });
-
-    return { messageValue: messageFromRequest };
-}
-interface ISendMessageResponse {
-    messageValue: string;
-}*/
-
-/*var MessageToPlayerOld = function (args: any, context: IPlayFabContext) {
-
-    var MAX_MESSAGES: number = 20;
-    var currentUserIndexData: PlayFabServerModels.UserDataRecord = server.GetUserData({ "PlayFabId": currentPlayerId, "Keys": ["currentInboxIndex"], }).Data["currentInboxIndex"];
-    var currentIndex: number;
-    if (currentUserIndexData) {
-        currentIndex = +currentUserIndexData.Value;
-    } else {
-        currentIndex = 0;
-    }
-
-    args.toPlayerId;
-    args.messageText;
-
-    // @TODO - store index of max message and add new messages after index.  later clean up all messages before index - 100
-    // new plan: create circular buffer of messages
-
-    var messageKey: string = "Message_" + currentIndex;
-    var fromKey: string = "FromPlayer_" + currentIndex;
-
-    var newIndex: number = ((currentIndex + 1) % MAX_MESSAGES);
-
-    var dataPayload = {};
-    dataPayload[messageKey] = args.messageText;
-    dataPayload[fromKey] = currentPlayerId;
-    dataPayload["currentInboxIndex"] = String(newIndex);
-
-    var updateDataRequest: PlayFabServerModels.UpdateUserDataRequest = {
-        PlayFabId: args.toPlayerId,
-        Data: dataPayload,
-        "Permission": "Private"
-    };
-
-    server.UpdateUserReadOnlyData(updateDataRequest);
-}*/
-
+ 
 class inboxMessage {
     sender: string;
     body: string;
@@ -72,61 +11,37 @@ class inboxMessage {
 }
 
 class playerInbox {
-    //@todo: add format version number?
-    inboxVersion: number = INBOX_VERSION;
     messages: inboxMessage[];
     constructor() { this.messages = new Array<inboxMessage>(0) } 
 }
 
-
 var GetInboxFromFile = function (targetEntity: PlayFabAuthenticationModels.EntityKey): playerInbox {
-    log.debug("GetMessagesFromFile called for entity: " + targetEntity.Id + " " + targetEntity.Type);
-    var targetInbox: playerInbox = new playerInbox();
+    //log.debug("GetMessagesFromFile called for entity: " + targetEntity.Id + " " + targetEntity.Type);
 
-    // var targetEntity: PlayFabDataModels.EntityKey = { "Id": playerId, "Type": "master_player_account" };
-    //var getFilesRequest: PlayFabDataModels.GetFilesRequest = { Entity: targetEntity };
+    var targetInbox: playerInbox = new playerInbox();
     var getFilesResult: PlayFabDataModels.GetFilesResponse = entity.GetFiles({ Entity: targetEntity });
     
-    log.debug("Got file list for Entity " + getFilesResult.Entity.Id);
+    //log.debug("Got file list for Entity " + getFilesResult.Entity.Id);
 
     if (getFilesResult.Metadata[INBOX_FILENAME] == null) {
-        log.debug("Player does not have an inbox, returning a new inbox.")
+        log.debug("Player does not have an inbox file, returning a new inbox.")
         return targetInbox;
     }
-
-    log.debug("Filename: " + getFilesResult.Metadata[INBOX_FILENAME].FileName);
-    log.debug("DownloadUrl: " + getFilesResult.Metadata[INBOX_FILENAME].DownloadUrl);
-    //log.debug("Size: " + getFilesResult.Metadata[INBOX_FILENAME].Size);
-    //log.debug("LastModified: " + getFilesResult.Metadata[INBOX_FILENAME].LastModified);
-
+    
     var fileURL: string = getFilesResult.Metadata[INBOX_FILENAME].DownloadUrl;
-
     var response = http.request(fileURL, "GET", "", 'application/json', {});
    
     if (response != null) {
         targetInbox = JSON.parse(response.toString()); //@todo: deal with parsing errors
     }
 
+    if (targetInbox == null) {
+        targetInbox = new playerInbox();
+    }
+
     return targetInbox;
 }
 
-/*var GetMessagesFromUserData = function (playerId: string): playerInbox {
-    log.debug("***** Deprecated GetMessagesFromUserData called ******");
-
-    var userDataResult: PlayFabServerModels.GetUserDataResult = server.GetUserInternalData({
-        "PlayFabId": playerId,
-        "Keys": [
-            INBOX_KEY
-        ],
-    });
-
-    // full contents of the recipient's inbox
-    var currentInbox: playerInbox = new playerInbox();
-    if (userDataResult.Data[INBOX_KEY]) {
-        currentInbox = JSON.parse(userDataResult.Data[INBOX_KEY].Value); //@todo: deal with parsing errors
-    }
-    return currentInbox;
-}*/
 
 var GetMessages = function (args: any, context: IPlayFabContext): playerInbox {
 
@@ -179,8 +94,6 @@ handlers["getMessages"] = GetMessages;
 
 function storeInbox(currentInbox: playerInbox, targetEntity: PlayFabAuthenticationModels.EntityKey) {
 
-    //var targetEntity: PlayFabDataModels.EntityKey = { "Id": targetPlayer, "Type": "master_player_account" }; //@todo: figure out master vs. title account usage here
-
     var initFileUploadRequest: PlayFabDataModels.InitiateFileUploadsRequest = {
         Entity: targetEntity,
         FileNames: [INBOX_FILENAME]
@@ -188,7 +101,7 @@ function storeInbox(currentInbox: playerInbox, targetEntity: PlayFabAuthenticati
 
     log.debug("Init File Upload FileNames: " + initFileUploadRequest.FileNames);
     
-    //initFileUploadRequest.ProfileVersion = oldVersion;//@todo - may want to do this
+    //initFileUploadRequest.ProfileVersion = oldVersion;//@todo - may want to do something with this - race condition here
     var initResponse: PlayFabDataModels.InitiateFileUploadsResponse = entity.InitiateFileUploads(initFileUploadRequest);
 
     log.debug("Init File Upload response: " + JSON.stringify(initResponse.UploadDetails));
@@ -205,11 +118,9 @@ function storeInbox(currentInbox: playerInbox, targetEntity: PlayFabAuthenticati
 
     var finalResponse: PlayFabDataModels.FinalizeFileUploadsResponse = entity.FinalizeFileUploads(finalFileUploadRequest);
 
-   // log.debug(finalResponse.Metadata[0].FileName + " uploaded. New LastModified = " + finalResponse.Metadata[0].LastModified);
-
 }
 
-// from: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+// based on this post: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
 function generateUUID() : string { // Public Domain/MIT 
     var d = new Date().getTime();
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -225,8 +136,7 @@ var SendMessage = function (args: any, context: IPlayFabContext): ISendMessageRe
 
     if (messageBody.length>1000) {
         log.error("Message body too large: " + messageBody.length );//@todo: use better check on message size < 1KB
-        return null;
-        // @todo: message error better
+        throw "Message exceeded maximum allowed size";
     }
 
     var messageRecipientId: string = args.toPlayerId;
@@ -304,8 +214,10 @@ interface IDeleteMessageResult {
 
 // deletes the current user's entire inbox
 var Truncate = function (args: any, context: IPlayFabContext) {
+    // @todo: add a check to ensure only administrators/services can call this function
+    // @todo: change this to take an ID of the account to truncate
+
     storeInbox(new playerInbox(), context.currentEntity.Entity);
-    log.debug("Truncate deleted the inbox for user " + context.currentEntity.Entity.Id);
 }
 handlers["truncate"] = Truncate;
 
@@ -321,17 +233,11 @@ handlers["easyLogEvent"] = EasyLogEvent;
 
 function getTitlePlayerEntityKey(masterPlayerID: string): PlayFabServerModels.EntityKey {
 
-    log.debug("getTitlePlayerEntityKey called for ID:" + masterPlayerID);
-
     var accountInfoResponse: PlayFabServerModels.GetUserAccountInfoResult = server.GetUserAccountInfo({ PlayFabId: masterPlayerID });
 
-    if (accountInfoResponse != null) {
-        log.debug("accountInfoResponse TitlePlayerAccount: " + accountInfoResponse.UserInfo.TitleInfo.TitlePlayerAccount.Id + ", " + accountInfoResponse.UserInfo.TitleInfo.TitlePlayerAccount.Type);
-        return accountInfoResponse.UserInfo.TitleInfo.TitlePlayerAccount;
+    if (accountInfoResponse == null) {
+        throw "Failed to get User Account Info for " + masterPlayerID;
     }
 
-    //@todo: handle this error case
-    log.debug("Maybe it's a title_player_account?");
-    // I give up, hard code it and hope it's a title_player_account.
-    return { Id: masterPlayerID, Type: "title_player_account" };
+    return accountInfoResponse.UserInfo.TitleInfo.TitlePlayerAccount;
 }
